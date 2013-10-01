88e59a26929e562217579d6b65d88ef945f2f99b
Evgeny Boykov <artscan@list.ru>
2013-10-01 18:31:16 +1100

    remove init_random_seed

--- phi.f90
+++ phi.f90
@@ -45,21 +45,6 @@ contains
     axes => axesi
   end subroutine set_axes
 
-  SUBROUTINE init_random_seed()
-    INTEGER :: i, n, clock
-    INTEGER, DIMENSION(:), ALLOCATABLE :: seed
-
-    CALL RANDOM_SEED(size = n)
-    ALLOCATE(seed(n))
-
-    CALL SYSTEM_CLOCK(COUNT=clock)
-
-    seed = clock + 37 * (/ (i - 1, i = 1, n) /)
-    CALL RANDOM_SEED(PUT = seed)
-
-    DEALLOCATE(seed)
-  END SUBROUTINE init_random_seed
-
   double precision function norm2(v)
     double precision, intent(in), dimension(:) :: v
     norm2 = sum(v(:)*v(:))
