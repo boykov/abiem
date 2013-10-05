8e73150f439a466ff122faf8f567d87f7672eb02
Evgeny Boykov <artscan@list.ru>
2013-10-05 12:14:08 +1100

    add class params (Phi)

--- testcase.py
+++ testcase.py
@@ -12,93 +12,70 @@ from dataelement import *
 from numpy import *
 from utilities import *
 from phi import modphi as phi
+from integ import modinteg as integ
 
-class testBIE(unittest.TestCase):
-    def setUp(self):
+class params():
+    def __init__(self, numpoints = 400, axes = [float(0.75),float(1.),float(0.5)]):
+        self.max_neighbors = 100
+        self.PI = 3.14159265358979324
+        self.nd = 3
+        self.axes = zeros((3), order = 'Fortran')
+        self.numpoints = numpoints
+        self.axes[:] = [float(0.75),float(1.),float(0.5)]
         pass
 
-    def scenario(self):
-        self.h = zeros((1), order = 'Fortran')
-        self.h2 = zeros((1), order = 'Fortran')
-        self.PI = zeros((1), order = 'Fortran')
+    def initQuad(self, orderquad):
+        self.data = DataElement(1)
+        self.data.orderquad = orderquad
+        self.data.setupquad()
+        self.Nz = self.data.orderquad
+        self.quadphi_over = zeros((self.Nz,2),order = 'Fortran')
+        self.quadphi_over[:,:] = self.data.quadphi_over[:,:]
 
-        self.nd = zeros((1), dtype = int32, order = 'Fortran')
-        self.Nz = zeros((1), dtype = int32, order = 'Fortran')
-        self.max_neighbors = zeros((1), dtype = int32, order = 'Fortran')
-
-        self.node_neighbors1 = zeros((self.numnodes,100), dtype = int32, order = 'Fortran')
-        self.node_neighbors2 = zeros((self.numnodes,100), dtype = int32, order = 'Fortran')
+    def initEllipsoid(self):
+        self.e = ellipsoid(self.axes, self.numpoints)
+        self.numnodes = self.e.points.shape[0]
+        self.h = self.e.get_h()
+        self.h2 = self.h * self.h
         self.node_coordinates = zeros((self.numnodes,3), order = 'Fortran')
         self.normal_coordinates = zeros((self.numnodes,3), order = 'Fortran')
-        self.nstroke_coordinates = zeros((self.numnodes,3), order = 'Fortran')
-        self.intphi_over = zeros((self.numnodes), order = 'Fortran')
-
-        self.centres = zeros((self.data.orderquad), order = 'Fortran')
-        self.C = zeros((self.data.orderquad), order = 'Fortran')
-        self.jacobian = zeros((4,self.data.orderquad,4*self.data.orderquad), dtype = complex, order = 'Fortran')
-        self.nodes = zeros((4,self.data.orderquad,4*self.data.orderquad,3), order = 'Fortran')
-
-        self.centres[:] = self.data.quadphi_over[:,0]
-        self.C[:] = self.data.quadphi_over[:,1]
-
-        self.area = zeros((1), order = 'Fortran')
-
-        self.h[0] = self.e.get_h()
-        self.max_neighbors[0] = 100
-        self.PI[0] = 3.14159265358979324
-        self.nd[0] = 3
         self.node_coordinates[:,:] = self.e.points[:,:]
         self.normal_coordinates[:,:] = self.e.normalvectors[:,:]
-        
-    def setObj(self,obj):
-        obj.set_nd(3)
-        obj.set_pi(3.14159265358979324)
-        obj.set_max_neighbors(100)
-        obj.set_h(self.e.get_h())
+
+    def initPhi(self):
+        self.node_neighbors1 = zeros((self.numnodes,100), dtype = int32, order = 'Fortran')
+        self.node_neighbors2 = zeros((self.numnodes,100), dtype = int32, order = 'Fortran')
+        self.nstroke_coordinates = zeros((self.numnodes,3), order = 'Fortran')
+
+        self.setObjPhi(phi)
+
+        phi.filter_neighbors(2*self.h, self.node_neighbors2, self.numnodes)
+        phi.filter_neighbors(  self.h, self.node_neighbors1, self.numnodes)
+        phi.normal_vector_stroke(self.numnodes, self.node_neighbors1)
+
+    def setObjPhi(self,obj):
+        obj.set_nd(self.nd)
+        obj.set_pi(self.PI)
+        obj.set_max_neighbors(self.max_neighbors)
+        obj.set_h(self.h)
         obj.set_numnodes(self.numnodes)
 
-        obj.set_node_coordinates(self.e.points)
-        obj.set_normal_coordinates(self.e.normalvectors)
+        obj.set_node_coordinates(self.node_coordinates)
+        obj.set_normal_coordinates(self.normal_coordinates)
         obj.set_nstroke_coordinates(self.nstroke_coordinates)
         obj.set_node_neighbors1(self.node_neighbors1)
         obj.set_node_neighbors2(self.node_neighbors2)
-        obj.set_axes(self.data.axes)
-        
-        obj.set_intphi_over(self.intphi_over)
-        obj.set_h2(self.e.get_h()*self.e.get_h())
-        obj.set_nz(self.data.orderquad)
 
-        obj.set_centres(self.centres)
-        obj.set_c(self.C)
-        obj.set_jacobian(self.jacobian)
-        obj.set_nodes(self.nodes)
-
-        obj.set_area(self.area)
-
-    def setEllipsoid(self):
-        self.e = ellipsoid(self.data.axes, self.data.numpoints)
-        self.numnodes = self.e.points.shape[0]
-
-    def setPhi(self):
-        self.setObj(phi)    
-
-        phi.filter_neighbors(2*self.e.get_h(), self.node_neighbors2, self.numnodes)
-        phi.filter_neighbors(  self.e.get_h(), self.node_neighbors1, self.numnodes)
-        phi.normal_vector_stroke(self.numnodes, self.node_neighbors1)
+class testBIE(unittest.TestCase):
+    def setUp(self):
+        self.P = params(400)
+        self.P.initQuad(20)
 
     def testEllipsoid(self):
-        self.data = DataElement(400)
-        self.data.orderquad = 20
-        self.data.setupquad()
-        self.setEllipsoid()
-        self.scenario()
-        self.assertAlmostEqual(center_points(self.e.points), 1.0e-11, places = 10)
+        self.P.initEllipsoid()
+        self.assertAlmostEqual(center_points(self.P.node_coordinates), 1.0e-11, places = 10)
 
     def testPhi(self):
-        self.data = DataElement(400)
-        self.data.orderquad = 20
-        self.data.setupquad()
-        self.setEllipsoid()
-        self.scenario()
-        self.setPhi()
-        self.assertAlmostEqual(phi.test_phi(gen_points(self.numnodes,self.data.axes)), self.numnodes, places = 12)
+        self.P.initEllipsoid()
+        self.P.initPhi()
+        self.assertAlmostEqual(phi.test_phi(gen_points(self.P.numnodes,self.P.axes)), self.P.numnodes, places = 12)
