b4f233d31a3799a9b46547df62775945e54c9e29
Evgeny Boykov <artscan@list.ru>
2014-02-04 14:27:41 +1100

    add steps_gmres

--- testcase.py
+++ testcase.py
@@ -49,6 +49,7 @@ class params():
         self.eps_matgen = 1e-4
         self.eps_aggl = self.eps_matgen
         self.eps_gmres = self.eps_matgen*0.01
+        self.steps_gmres = 20*self.numpoints
         self.eta = 0.8
         self.bmin = 15
         self.rankmax = 1000
@@ -64,6 +65,7 @@ class params():
         slaeahmed.set_Hmatrix(self.eps_matgen,
                               self.eps_aggl,
                               self.eps_gmres,
+                              self.steps_gmres,
                               self.eta,
                               self.bmin,
                               self.rankmax)
