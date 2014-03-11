91913121364c0731e95283f8402e018098312b03
Evgeny Boykov <artscan@list.ru>
2014-03-11 15:41:57 +1100

    add testlimA, change comparison limA, sigma, intphi_under

--- temp.py
+++ temp.py
@@ -1,14 +1,18 @@
-j = 0
-print "sigma(1) = ", self.P.sigma[j]
-print "phi = ", self.P.intphi_over[j]
-limA = self.P.intphi_over[j]/((2*math.pi)**(1.5)*self.P.sigma[j])
-print "phi*limA = ", limA
-phi_r = self.P.intphi_under[j]/(4*math.pi)
-print "phi/(4pi*r) = ", phi_r
-print "*/* =", limA/phi_r
-print "sum = ", sum(self.P.intphi_under[:])/(4*math.pi)
-print "φ(r)/r full =", 0.691797810029
-print "φ(r)/r - sum =", sum(self.P.intphi_under[:])/(4*math.pi) - self.P.gauss[0,2]
+def testlimA(self):
+    j = 0
+    sigma = math.sqrt(2*math.pi)*math.sqrt(0.5*self.P.intphi_over[j]/(math.pi**2))
+    sigma_new = 2*self.P.intphi_over[j]/math.sqrt(2*math.pi)/self.P.intphi_under[j] # chiterstvo
+    print "sigma(1) = ", self.P.sigma[j], sigma, sigma_new
+    print "h = ", self.P.hval
+    print "phi = ", self.P.intphi_over[j]
+    limA = self.P.intphi_over[j]/((2*math.pi)**(1.5)*sigma)
+    print "phi*limA = ", limA
+    phi_r = self.P.intphi_under[j]/(4*math.pi)
+    print "phi/(4pi*r) = ", phi_r
+    print "*/* =", limA/phi_r
+    print "sum = ", sum(self.P.intphi_under[:])/(4*math.pi)
+    print "φ(r)/r full =", 0.691797810029
+    print "φ(r)/r - sum =", sum(self.P.intphi_under[:])/(4*math.pi) - self.P.gauss[0,2]
 
 self.centres[:] = self.quadphi_under[:,0]
 self.C[:] = self.quadphi_under[:,1]
@@ -17,12 +21,6 @@ integ.calcomp2()
 self.centres[:] = self.quadphi_over[:,0]
 self.weights[:] = self.quadphi_over[:,1]
 integ.calcomp3()
-
-def testUnder(self):
-    print (self.P.intphi_under[0] + sum(self.P.gauss[:,5]))/(4*math.pi)
-    print self.P.gauss[0,3]
-    print self.P.gauss[0,3] - sum(self.P.gauss[:,5])/(4*math.pi)
-    print (self.P.intphi_under[0])/(4*math.pi)
 
 [self.intphi_over[:]] = self.withWrapMemo([self.axes,
                                            self.node_coordinates,
