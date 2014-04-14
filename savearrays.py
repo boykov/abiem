e0867a8913c35359e1ef093e8fc5c7d029e5a620
Evgeny Boykov <artscan@list.ru>
2014-04-14 13:56:14 +1100

    add savearrays.py

---
+++ savearrays.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+__author__ = "Evgeny Boykov. <artscan@list.ru>"
+
+import math
+import cmath
+import pickle
+import shelve
+import sys,os
+from numpy import *
